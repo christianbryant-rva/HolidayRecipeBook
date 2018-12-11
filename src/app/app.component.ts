e8188064541a6e37342c7a31a0153f79ee18e07f
XtianB <christianbryantmusic@gmail.com>
2018-12-11 05:22:30 -0800

    Adding Recipe Directive and Header Component to new project -cwb 12112018

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [HeaderComponent, RecipesComponent],
 })
 export class AppComponent {
   title = 'app works!';
